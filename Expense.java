1a669fae0f0455337ed248f5054e08548a04801a
Nguyen Hoang Trung <62344090+brainiac004@users.noreply.github.com>
2020-09-20 21:49:24 +0700

    Update Expense.java

--- Expense.java
+++ Expense.java
@@ -5,6 +5,8 @@
  */
 package expense;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Scanner;
 
@@ -64,13 +66,26 @@ public class Expense {
         }
     }
 
-    public static void addElements(ArrayList<Maneger> o,int id) {
-        
-        System.out.println("Enter Date:");
-        String date = checkInputString();
-        System.out.println("Enter Amount:");
+    public static String checkDate() {
+        while (true) {
+            String d = sc.nextLine().trim();
+            SimpleDateFormat df = new SimpleDateFormat("dd-MM-yyyy");
+            df.setLenient(false);
+            try {
+                df.parse(d);
+                return d;
+            } catch (Exception e) {
+                System.err.println("Invalid date");
+            }
+        }
+    }
+
+    public static void addElements(ArrayList<Maneger> o, int id) {
+        System.out.print("Enter Date:");
+        String date = checkDate();
+        System.out.print("Enter Amount:");
         int amount = checkInputInteger();
-        System.out.println("Enter Content:");
+        System.out.print("Enter Content:");
         String content = checkInputString();
         o.add(new Maneger(id, date, amount, content));
 
@@ -82,12 +97,14 @@ public class Expense {
         }
         double total = 0;
         System.out.println("-------Display all expenses-------");
-        System.out.printf("-7d%-20s%-20s%-20s%\n", "ID", "Date", "Amount", "Content");
+        System.out.printf("%7s%20s%20s%20s\n", "ID", "Date", "Amount", "Content");
         for (int i = 0; i < o.size(); i++) {
-            System.out.printf("-7d%-20s%-20f%-20s%", o.get(i).getID(), o.get(i).getDate(), o.get(i).getContent());
+            System.out.printf("%7d%20s%20f%20s\n",o.get(i).getID(),o.get(i).getDate(),o.get(i).getNumber(),o.get(i).getContent());
             total += o.get(i).getNumber();
         }
-        System.out.printf("Total:%-20f\n", total);
+        System.out.println("");
+        System.out.print("Total:"+total);
+        System.out.println("");
     }
 
     public static boolean checkID(ArrayList<Maneger> o, int id) {
@@ -98,43 +115,51 @@ public class Expense {
         }
         return false;
     }
-    public static void deleteElements(ArrayList<Maneger> o){
-        System.out.print("Enter id:");
+
+    public static void deleteElements(ArrayList<Maneger> o) {
+        System.out.println("Enter Id check: ");
         int id = checkInputInteger();
-        if(checkID(o, id)== true){
-            o.remove(id);
-            System.out.println("Delete successful");
-            
-        }else{
-            System.out.println("Delete fail!");
+        for (int i = 0; i < o.size(); i++) {
+            if (o.get(i).getID()==id) {
+                o.remove(i);
+                System.out.println("delete susscesful!");
+                break;
+            } else {
+                System.out.println("delete fail");
+            }
+  
         }
+        
     }
 
     public static void displayResult() {
         ArrayList<Maneger> o = new ArrayList<>();
-        int id =0;
-        while(true){
-        System.out.println("=======Handy Expense program=======");
-        System.out.println("1. Add an expense");
-        System.out.println("2. Display all expenses");
-        System.out.println("3. Delete an expense");
-        System.out.println("Quit");
-        System.out.println("Your choice:");
-        int choice = checkInputChoice(1, 4);
-        switch (choice) {
-            case 1:addElements(o, ++id);
-                break;
-            case 2: displayElements(o);
-                break;
-            case 3:deleteElements(o);
-                break;
-            case 4:
-                return;
-        }
+        int id = 0;
+        while (true) {
+            System.out.println("=======Handy Expense program=======");
+            System.out.println("1. Add an expense");
+            System.out.println("2. Display all expenses");
+            System.out.println("3. Delete an expense");
+            System.out.println("4. Quit");
+            System.out.print("Your choice:");
+            int choice = checkInputChoice(1, 4);
+            switch (choice) {
+                case 1:
+                    addElements(o, ++id);
+                    break;
+                case 2:
+                    displayElements(o);
+                    break;
+                case 3:
+                    deleteElements(o);
+                    break;
+                case 4:
+                    return;
+            }
         }
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         displayResult();
     }
 }
