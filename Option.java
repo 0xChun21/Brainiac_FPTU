b0d3f1363f16378a587d73876858d986ada03f0d
Nguyen Hoang Trung <62344090+brainiac004@users.noreply.github.com>
2020-09-27 23:58:07 +0700

    Add files via upload

--- Option.java
+++ Option.java
@@ -1,65 +1,159 @@
-
-import java.util.ArrayList;
-import java.util.Scanner;
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
+package Manager;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
 /**
  *
- * @author trinh
+ * @author Dell
  */
-public class Option implements IOption {
+public class Option {
 
-    @Override
-    public void display(ArrayList<Expense> list) {
-        System.out.printf("%2s%20s%20s%20s", "ID", "Date", "Amount", "Content\n");
-        for (Expense o : list) {
-            System.out.println(o);
-        }
-    }
+    static Scanner sc = new Scanner(System.in);
+    static final String ACCOUNT_NUMBER_VALID = "^\\d{10}$";
+    static final char[] chars = {'1', 'A', 'a', 'B', 'b', 'C',
+        'c', '2', 'D', 'd', 'E', 'e', 'F', 'f', '3', 'G', 'g', 'H', 'h',
+        'I', 'i', 'J', 'j', 'K', 'k', 'L', 'l', '4', 'M', 'm', 'N', 'n',
+        'O', 'o', '5', 'P', 'p', 'Q', 'q', 'R', 'r', 'S', 's', 'T', 't',
+        '6', '7', 'U', 'u', 'V', 'v', 'U', 'u', 'W', 'w', '8', 'X', 'x',
+        'Y', 'y', 'Z', 'z', '9'};
 
-    @Override
-    public void add(ArrayList<Expense> list) {
-        Validate v = new Validate();
-        Scanner sc = new Scanner(System.in);
-        String date = v.getDate();
-        System.out.print("input amount: ");
-        double amount = Double.parseDouble(sc.nextLine());
-        System.out.print("input content: ");
-        String content = sc.nextLine();
-
-        int last = list.size() - 1;
-        int id;
-        if (last == -1) {
-            id = 1;
-        } else {
-            id = list.get(last).getId() + 1;
-        }
-        Expense e = new Expense(id, date, amount, content);
-        list.add(e);
-    }
-
-    @Override
-    public void remove(ArrayList<Expense> list) {
-        Scanner sc = new Scanner(System.in);
-        int id;
-        System.out.print("input id: ");
-        id = Integer.parseInt(sc.nextLine());
-        boolean check = true;
-        for (int i = 0; i < list.size(); i++) {
-            if (list.get(i).getId() == id) {
-                list.remove(i);
-                check = false;
-                System.out.println("Delete an expense successful");
-                break;
+    public static int checkRanger(int min, int max, Locale language) {
+        while (true) {
+            try {
+                int n = Integer.parseInt(sc.nextLine());
+                if (n < min || n > max) {
+                    throw new NumberFormatException();
+                }
+                return n;
+            } catch (NumberFormatException e) {
+                getWordLanguage(language, "errorCheckInputIntLimit");
+   
             }
         }
-        if (check) {
-            System.out.println("Delete an expense fail");
+    }
+
+    public static String checkString(Locale Language) {
+        while (true) {
+            String n = sc.nextLine();
+            if (n.isEmpty()) {
+                getWordLanguage(Language, "errCheckInputIntLimit");
+            }
+            return n;
         }
+    }
+
+    public static int checkAccount(Locale language) {
+        while (true) {
+            String n = sc.nextLine().trim();
+            if (n.matches(ACCOUNT_NUMBER_VALID)) {
+                return Integer.parseInt(n);
+            }
+            else{
+                getWordLanguage(language, "errCheckInputAccount");
+            }
+        }
+
+    }
+
+    public static String checkPassword(Locale languge) {
+        while (true) {
+            String pass = sc.nextLine();
+            int countD = 0, countL = 0;
+            for (int i = 0; i < pass.length(); i++) {
+                if (Character.isDigit(pass.charAt(i))) {
+                    countD++;
+                }
+                if (Character.isLetter(pass.charAt(i))) {
+                    countL++;
+                }
+                if (pass.isEmpty()) {
+                    System.out.println("Error! reinput");
+                }
+                if (pass.length() > 6 && pass.length() < 15 && countD >= 1 && countL >= 1) {
+                    return pass;
+                } else {
+                    System.out.println("Error!");
+                }
+            }
+
+        }
+    }
+
+    static boolean checkInputCaptcha(String captchaGenerated, Locale language) {
+        System.out.println(captchaGenerated);
+        getWordLanguage(language, "enterCaptcha");
+        String captchaInput = checkString(language);
+        for (int i = 0; i < captchaInput.length(); i++) {
+            if (!captchaGenerated.contains(Character.toString(captchaInput.charAt(i)))) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String generateCaptchaText() {
+        String randomStrValue = "";
+        final int LENGTH = 6;
+        StringBuilder sb = new StringBuilder();
+        int index = 0;
+        for (int i = 0; i < LENGTH; i++) {
+            index = (int) (Math.random() * (chars.length - 1));
+            sb.append(chars[index]);
+        }
+        return sb.toString();
+    }
+
+    public static void login(Locale language) {
+        getWordLanguage(language, "Enter AccountNumber: ");
+        int acc = checkAccount(language);
+        getWordLanguage(language, "Enter password: ");
+        String pas = checkPassword(language);
+        String cap = generateCaptchaText();
+        while (true) {            
+            if(checkInputCaptcha(cap, language)){
+                getWordLanguage(language,"login success");
+            }else{
+                getWordLanguage(language, "errCaptchaIncorrect");
+            }
+        }
+
+    }
+
+    static void getWordLanguage(Locale curLocate, String key) {
+        ResourceBundle words = ResourceBundle.getBundle("Language/" + curLocate, curLocate);
+        String value = words.getString(key);
+        System.out.printf(value);
+    }
+
+    public static void display() {
+        Locale vietnamese = new Locale("vi");
+        Locale english = Locale.ENGLISH;
+        System.out.println("1. Vietnamese");
+        System.out.println("2. English");
+        System.out.println("3. Exit");
+        System.out.println("Your choice: ");
+        int choice = checkRanger(1, 3, english);
+        switch(choice){
+            case 1:
+                login(vietnamese);
+                break;
+            case 2:
+                login(english);
+                break;
+            case 3:
+                return;
+        }
+    }
+
+    public static void main(String[] args) {
+        display();
     }
 
 }
